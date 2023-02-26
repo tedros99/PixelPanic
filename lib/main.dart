ba2d6fa848ea96d1f176d254f2cce17d665007b7
hbjlakf;eial <thisisntragebox@gmail.com>
2023-02-26 13:39:53 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget topSection = SizedBox(
+      width: 300,
+      height: 450,
+      child: Card(
+          color: Color.fromARGB(255, 189, 199, 144),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "TetriField",
+                textScaleFactor: 3.0,
+              ),
+              ElevatedButton(
+                onPressed: () => print("PLAY"),
+                child: const Text(
+                  "PLAY",
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 40, 43, 31),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => print("RULES"),
+                child: const Text("RULES"),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 40, 43, 31),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => print("SCORE"),
+                child: const Text("SCORE"),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 40, 43, 31),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => print("EXIT"),
+                child: const Text("EXIT"),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 40, 43, 31),
+                ),
+              ),
+            ],
+          )),
+    );
+
+    Widget dpad = GridView.count(
+      crossAxisCount: 3,
+      children: [
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+            color: Colors.black,
+            child: RotatedBox(
+              quarterTurns: 1,
+              child: IconButton(
+                color: Colors.white,
+                icon: const Icon(Icons.arrow_left),
+                onPressed: () => print("dpad up"),
+              ),
+            )),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: Colors.black,
+          child: IconButton(
+            color: Colors.white,
+            icon: const Icon(Icons.arrow_left),
+            onPressed: () => print("dpad left"),
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: Colors.black,
+          child: IconButton(
+            color: Colors.white,
+            icon: const Icon(Icons.arrow_right),
+            onPressed: () => print("dpad right"),
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+            color: Colors.black,
+            child: RotatedBox(
+              quarterTurns: 3,
+              child: IconButton(
+                color: Colors.white,
+                icon: const Icon(Icons.arrow_left),
+                onPressed: () => print("dpad up"),
+              ),
+            )),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        )
+      ],
+    );
+
+    Widget swapBtn = GridView.count(
+      crossAxisCount: 3,
+      children: [
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: Colors.grey,
+          child: IconButton(
+            icon: const Icon(Icons.swap_horiz),
+            onPressed: () => print("swap butn"),
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        )
+      ],
+    );
+
+    Widget abBtn = GridView.count(
+      crossAxisCount: 2,
+      children: [
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+          child: IconButton(
+            color: Colors.red,
+            iconSize: 48.00,
+            icon: const Icon(Icons.circle),
+            onPressed: () => print("swap butn"),
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+          child: IconButton(
+            color: Colors.blue,
+            iconSize: 48.00,
+            icon: const Icon(Icons.circle),
+            onPressed: () => print("swap butn"),
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 204, 214, 221),
+        ),
+      ],
+    );
+
+    Widget controls = GridView.count(
+      crossAxisCount: 3,
+      children: [
+        Container(
+          color: Colors.white,
+          child: dpad,
+        ),
+        Container(
+          color: Colors.white,
+          child: swapBtn,
+        ),
+        Container(
+          color: Colors.white,
+          child: abBtn,
+        )
+      ],
+    );
+
+    Widget bottomSection = SizedBox(
+      width: 350,
+      height: 200,
+      child: controls,
+    );
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 204, 214, 221),
+      ),
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.only(top: 32),
+          alignment: Alignment.center,
+          child: Column(children: [topSection, bottomSection]),
+        ),
+      ),
+    );
+  }
+}
