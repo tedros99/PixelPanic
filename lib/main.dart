88cdc99ae855db461dd91e1ee8b70c18ec5654d9
hbjlakf;eial <thisisntragebox@gmail.com>
2023-04-08 15:44:52 -0400

    Game update

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Pixel Panic",
+                "PixelPanic",
                 textScaleFactor: 3.0,
                 style: GoogleFonts.robotoMono(),
               ),
